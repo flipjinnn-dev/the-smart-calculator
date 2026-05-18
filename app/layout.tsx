83a0c130fecfe8c40122d34957104b4b91c3f7ee
tamatosdev <tamatos.dev@gmail.com>
2026-05-18 19:13:17 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,7 +152,7 @@ export default async function RootLayout({
   return (
     <html lang={htmlLang}>
       <head dangerouslySetInnerHTML={{
-        __html: `<script data-grow-initializer="">!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo5YWQ2OTQ2OC1hNTA4LTQ5ZWMtOWM0Ny05ZGUxYjhkZmNmYWQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();</script>`
+        __html: ``
       }} />
       <body className={inter.className}>
         <SessionProvider>
